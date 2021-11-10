7c651829758b60c8d88c9d1560086b5b28628b99
zr <247076126@qq.com>
2021-11-10 11:42:01 +0800

    🦄 refactor(项目结构):
    修改多项目结构

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/packages/__tests__/**/*.(ts|js)',
+    '<rootDir>/packages/es/__tests__/**/*.(ts|js)',
   ],
 };
