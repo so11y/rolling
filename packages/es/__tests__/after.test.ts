61944ea2520f11b34f69f7046a2703881511bfde
zr <247076126@qq.com>
2021-11-10 23:54:17 +0800

    1

--- packages/es/__tests__/after.test.ts
+++ packages/es/__tests__/after.test.ts
@@ -5,12 +5,8 @@ import runners from '../runners';
 test('test function after', () => {
 	const callback = jest.fn();
 	const runner = after(2, callback);
-	//给runner绑定mockThis指向
 	const bindhaveThisRunner = bindMockThis(mockThis, runner);
-	//循环执行绑定完成的回调函数3次
 	runners(3, () => bindhaveThisRunner());
-	//期望callback只执行了一次
 	expect(callback).toBeCalledTimes(1);
-	//判断callback的this指向mockThis
 	expect(callback.mock.instances[0]).toBe(mockThis);
 });
