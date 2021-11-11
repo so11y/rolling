911e1d06dfaa35470eaa1e6dc60d7c9a367e98e1
hongpengcheng <danieleuoov@163.com>
2021-11-11 10:30:14 +0800

    🎈 perf(isfunction.test.ts): 添加isfunction测试用例

--- packages/es/__tests__/isFuntion.test.ts
+++ packages/es/__tests__/isFuntion.test.ts
@@ -4,5 +4,6 @@ test('test isFunction', () => {
 	const callback = jest.fn();
 
 	expect(isFunction(callback)).toBeTruthy();
+	expect(isFunction(1)).toBeFalsy();
 	expect(isFunction({})).toBeFalsy();
 });
