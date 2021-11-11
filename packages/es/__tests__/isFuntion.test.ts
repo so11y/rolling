fd1016508023db8070a211b706da79e57ff98fb3
hongpengcheng <danieleuoov@163.com>
2021-11-11 15:10:27 +0800

    ✨ feat(1223123):

--- packages/es/__tests__/isFuntion.test.ts
+++ packages/es/__tests__/isFuntion.test.ts
@@ -2,8 +2,8 @@ import isFunction from '../isFunction';
 
 test('test isFunction', () => {
 	const callback = jest.fn();
-
 	expect(isFunction(callback)).toBeTruthy();
 	expect(isFunction(1)).toBeFalsy();
 	expect(isFunction({})).toBeFalsy();
+    expect(isFunction(null)).toBeFalsy();
 });
