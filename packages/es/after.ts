6494db4cbebafcd050a22d6031439be40918e81a
2470761262 <247076126@qq.com>
2021-11-12 16:49:14 +0800

    feat(111): 2222
    333

--- packages/es/after.ts
+++ packages/es/after.ts
@@ -3,7 +3,7 @@ import { TNoCare } from './share/types';
 const after = (count: number, callback: TNoCare) => {
 	let _count = 0;
 	return function (this: unknown) {
-		if (--_count === count) callback.call(this);
+		if (++_count === count) callback.call(this);
 	};
 };
 
