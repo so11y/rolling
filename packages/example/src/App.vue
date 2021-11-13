a5ac8bd58354b2e29d7b71b497e6323e2c582ba6
钢琴家 <247076126@qq.com>
2021-11-13 18:39:29 +0800

    feat(示例):
    添加示例

---
+++ packages/example/src/App.vue
@@ -0,0 +1,15 @@
+<script  lang="ts">
+import Layout from './components/Layout.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	components: {
+		Layout
+	}
+});
+</script>
+
+<template><Layout /></template>
+
+<style lang="scss">
+</style>
---
+++ packages/example/src/components/Layout.vue
@@ -0,0 +1,18 @@
+<style scoped>
+.app-aside {
+	width: theme('width.44');
+	height: calc(theme('width.screen') - theme('height.14'));
+}
+</style>
+<template>
+	<div class="h-14 shadow-lg flex items-center pl-4 pr-4">TS-Lodash</div>
+	<div class="app-aside w-44"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {}
+});
+</script>
